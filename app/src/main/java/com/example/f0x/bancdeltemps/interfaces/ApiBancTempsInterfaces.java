ac9ba81a5c444241908e4e8d7cc0771a2b58fc84
Alex <Alex@DESKTOP-KH2V179>
2019-04-04 20:00:40 +0200

    LoginActivity creat y Carpetes creades

---
+++ app/src/main/java/com/example/f0x/bancdeltemps/interfaces/ApiBancTempsInterfaces.java
@@ -0,0 +1,33 @@
+package com.example.f0x.bancdeltemps.interfaces;
+
+import com.example.f0x.bancdeltemps.responses.*;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface ApiBancTempsInterfaces {
+
+//    @FormUrlEncoded
+//    @POST("auth/login")
+//    Call<ResponseLogin> doLogin(@Field("email") String email,
+//                                @Field("password") String password);
+//
+//    @GET("averia/lista")
+//    Call<ResponseLlistatAveries> getAverias(@Query("X-API-KEY") String key);
+//
+//    @GET("taller/lista")
+//    Call<ResponseLlistatTallers> getTallers(@Query("X-API-KEY") String key);
+//
+//    @FormUrlEncoded
+//    @POST("auth/register")
+//    Call<ResponseRegister> doRegister(@Field("nombre") String nombre,
+//                                      @Field("email") String email,
+//                                      @Field("password") String password);
+//
+//    @FormUrlEncoded
+//    @POST("averia/nueva")
+//    Call<ResponseAfegirAveria> afegirAveria(@Field("X-API-KEY") String key,
+//                                            @Field("titulo") String titul,
+//                                            @Field("descripcion") String descripcio,
+//                                            @Field("marca") String marca,
+//                                            @Field("modelo") String model);
+}
